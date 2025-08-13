739751abfa7960c4b482983ffc6589a8e845c492
Rastapopulus619 <bg.bahasajerman@google.com>
2025-08-13 22:37:50 +0700

    trying to get rendering to run

--- Models/LessonCardTableDataModel.cs
+++ Models/LessonCardTableDataModel.cs
@@ -1,16 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace bgbahasajerman_RazorClassLibrary.Models
+﻿namespace bgbahasajerman_RazorClassLibrary.Models
 {
     public class LessonCardTableDataModel
     {
+        // if Date == default(DateTime), we treat the cell as "empty"
         public DateTime Date { get; set; }
+
+        // did the student attend this lesson?
         public bool Attended { get; set; }
+
+        // was this lesson replaced by another date?
         public bool Replaced { get; set; }
+
+        // the replacement date (only meaningful when Replaced == true)
         public DateTime? ReplacementDate { get; set; }
     }
 }
